4edb871553ca1ac67a517f3896dcd34c4ddfc8a0
Paul Wagner <wagnerpj@uwec.edu>
2020-10-29 13:20:18 -0500

    Fix sample submission files 1-50
    Replaced lt_s01.sql through lt_s50.sql in /files subfolder - old instructor comment markers were used, breaking parsing.  New files should parse correctly under SQLFE.

--- files/lt_s11.sql
+++ files/lt_s11.sql
@@ -1,17 +1,17 @@
---- CS 260, Fall 2019, Lab Test
---- Name: Student 11
+-- -- CS 260, Fall 2019, Lab Test
+-- -- Name: Student 11
 
---- Table List:
---- Customer ( _CustID_ , FName, LName, PIN)
---- Account ( _AccNumber_ , AccType, AccBalance, Customer (FK), AccOpenLocation, 
----                         AccOpenDate, AccClosedDate, AccStatus)
---- Transaction ( _TransID_ , AccNumber (FK), TransAmount, TransDateTime, 
----                         TransType, TransLocation)
+-- -- Table List:
+-- -- Customer ( _CustID_ , FName, LName, PIN)
+-- -- Account ( _AccNumber_ , AccType, AccBalance, Customer (FK), AccOpenLocation, 
+-- --                         AccOpenDate, AccClosedDate, AccStatus)
+-- -- Transaction ( _TransID_ , AccNumber (FK), TransAmount, TransDateTime, 
+-- --                         TransType, TransLocation)
 
 
---- 1. (15 points) List the customer id, both name fields, and the account closed 
----    date for customers whose accounts opened in the Central location but have 
----    been closed on or after March 1st, 2017.
+-- -- 1. (15 points) List the customer id, both name fields, and the account closed 
+-- --    date for customers whose accounts opened in the Central location but have 
+-- --    been closed on or after March 1st, 2017.
 SELECT C.custID, C.FName, C.LName, A.AccClosedDate
 FROM Customer C
     JOIN Account A
@@ -20,23 +20,23 @@ FROM Customer C
     AND A.AccClosedDate >= '01-MAR-17';
 
 
---- 2. (16 points) For each account opening location, show how many active, closed,
----    and frozen, and accounts there are.  Display the results with the locations
----    and account status values in alphabetical order.
----        EXAMPLE OUTPUT (not real numbers - just shown to illustrate format)
----        Central        Active        10
----        Central        Closed        20
----        Central        Frozen         1
----        <similar for other branches>
+-- -- 2. (16 points) For each account opening location, show how many active, closed,
+-- --    and frozen, and accounts there are.  Display the results with the locations
+-- --    and account status values in alphabetical order.
+-- --        EXAMPLE OUTPUT (not real numbers - just shown to illustrate format)
+-- --        Central        Active        10
+-- --        Central        Closed        20
+-- --        Central        Frozen         1
+-- --        <similar for other branches>
 SELECT A.AccOpenLocation, A.AccStatus, COUNT(A.AccStatus)
     FROM Account A
     GROUP BY A.AccOpenLocation, A.AccStatus
     ORDER BY a.accopenlocation, A.AccStatus;
 
 
---- 3. (18 points) List the accounts (by account number, in numeric order) 
----    with an account balance greater than the average account balance 
----    for all accounts of the same account type (checking or savings).
+-- -- 3. (18 points) List the accounts (by account number, in numeric order) 
+-- --    with an account balance greater than the average account balance 
+-- --    for all accounts of the same account type (checking or savings).
 SELECT A.AccNumber
     FROM Account A
     WHERE A.accbalance >
@@ -46,13 +46,13 @@ SELECT A.AccNumber
             ORDER BY A.accnumber;
 
 
---- 4. (18 points) Display the transaction location and the average transaction 
----    amount for each transaction location where that average transaction amount
----    is less than the average transaction amount for all transactions.  Order the
----    results by average transaction amount, largest to smallest.
---- NOTE: for cleaner display, you can (but do not need to do so) CAST a real 
----    number to display with a limited number of decimal places; 
----    e.g. CAST (AVG(value) AS NUMBER(*,2)) 
+-- -- 4. (18 points) Display the transaction location and the average transaction 
+-- --    amount for each transaction location where that average transaction amount
+-- --    is less than the average transaction amount for all transactions.  Order the
+-- --    results by average transaction amount, largest to smallest.
+-- -- NOTE: for cleaner display, you can (but do not need to do so) CAST a real 
+-- --    number to display with a limited number of decimal places; 
+-- --    e.g. CAST (AVG(value) AS NUMBER(*,2)) 
 SELECT T.transLocation, AVG(T.transAmount)
     FROM Transaction T
     GROUP BY T.transLocation
@@ -63,10 +63,10 @@ SELECT T.transLocation, AVG(T.transAmount)
         ORDER BY AVG(T.transAmount) DESC;
 
 
---- 5. (16 points) Find each customer id, first name, and last name, plus the 
----    amount for the largest 'w' (withdrawal) transaction related to a 
----    savings account which that customer has opened.  Display the results 
----    in order by customer id.
+-- -- 5. (16 points) Find each customer id, first name, and last name, plus the 
+-- --    amount for the largest 'w' (withdrawal) transaction related to a 
+-- --    savings account which that customer has opened.  Display the results 
+-- --    in order by customer id.
 SELECT C.custID, C.FName, C.LName, MAX(T.transAmount)
     FROM Customer C
     JOIN Account A 
@@ -77,10 +77,10 @@ SELECT C.custID, C.FName, C.LName, MAX(T.transAmount)
         GROUP BY C.custID,C.FName, C.LName, T.transAmount
         ORDER BY C.custID;
 
---- 6. (17 points) List the customer id, first name and last name of any 
----    customers who have no active accounts.  This includes A) customers who 
----    have accounts, but they only have accounts that are closed and/or frozen, 
----    and also B) customers who do not have any accounts at all.
+-- -- 6. (17 points) List the customer id, first name and last name of any 
+-- --    customers who have no active accounts.  This includes A) customers who 
+-- --    have accounts, but they only have accounts that are closed and/or frozen, 
+-- --    and also B) customers who do not have any accounts at all.
 SELECT C.custID, C.FName, C.LName
     FROM Customer C
    JOIN Account A
@@ -92,10 +92,10 @@ SELECT C.custID, C.FName, C.LName
     WHERE NOT EXISTS
         (SELECT * FROM Account A
         WHERE C.custID = A.Customer);
---- 7. EXTRA CREDIT / ANSWER OPTIONAL (5 points): For each transaction location, 
----    display the number of accounts opened at that location, even if no 
----    accounts were opened at that location.
---- NOTE: at least one transaction location has no accounts opened at that location
+-- -- 7. EXTRA CREDIT / ANSWER OPTIONAL (5 points): For each transaction location, 
+-- --    display the number of accounts opened at that location, even if no 
+-- --    accounts were opened at that location.
+-- -- NOTE: at least one transaction location has no accounts opened at that location
 SELECT T.transLocation, COUNT(T.transID)
      FROM Transaction T
      GROUP BY T.transLocation;
--- files/lt_s39.sql
+++ files/lt_s39.sql
@@ -1,17 +1,17 @@
---- CS 260, Fall 2019, Lab Test
---- Name: Student 39
+-- -- CS 260, Fall 2019, Lab Test
+-- -- Name: Student 39
 
---- Table List:
---- Customer ( _CustID_ , FName, LName, PIN)
---- Account ( _AccNumber_ , AccType, AccBalance, Customer (FK), AccOpenLocation, 
----                         AccOpenDate, AccClosedDate, AccStatus)
---- Transaction ( _TransID_ , AccNumber (FK), TransAmount, TransDateTime, 
----                         TransType, TransLocation)
+-- -- Table List:
+-- -- Customer ( _CustID_ , FName, LName, PIN)
+-- -- Account ( _AccNumber_ , AccType, AccBalance, Customer (FK), AccOpenLocation, 
+-- --                         AccOpenDate, AccClosedDate, AccStatus)
+-- -- Transaction ( _TransID_ , AccNumber (FK), TransAmount, TransDateTime, 
+-- --                         TransType, TransLocation)
 
 
---- 1. (15 points) List the customer id, both name fields, and the account closed 
----    date for customers whose accounts opened in the Central location but have 
----    been closed on or after March 1st, 2017.
+-- -- 1. (15 points) List the customer id, both name fields, and the account closed 
+-- --    date for customers whose accounts opened in the Central location but have 
+-- --    been closed on or after March 1st, 2017.
 SELECT CUSTID,FNAME,LNAME,AccClosedDate
 FROM CUSTOMER C
 JOIN ACCOUNT A ON (A.CUSTOMER = C.CUSTID)
@@ -20,23 +20,23 @@ AND AccOpenLocation = 'Central'
 AND AccClosedDate >= '01-MAR-2017';
 
 
---- 2. (16 points) For each account opening location, show how many active, closed,
----    and frozen, and accounts there are.  Display the results with the locations
----    and account status values in alphabetical order.
----        EXAMPLE OUTPUT (not real numbers - just shown to illustrate format)
----        Central        Active        10
----        Central        Closed        20
----        Central        Frozen         1
----        <similar for other branches>
+-- -- 2. (16 points) For each account opening location, show how many active, closed,
+-- --    and frozen, and accounts there are.  Display the results with the locations
+-- --    and account status values in alphabetical order.
+-- --        EXAMPLE OUTPUT (not real numbers - just shown to illustrate format)
+-- --        Central        Active        10
+-- --        Central        Closed        20
+-- --        Central        Frozen         1
+-- --        <similar for other branches>
 SELECT AccOpenLocation,AccStatus,COUNT(*)
 FROM ACCOUNT
 GROUP BY AccOpenLocation,AccStatus
 ORDER BY AccOpenLocation,AccStatus;
 
 
---- 3. (18 points) List the accounts (by account number, in numeric order) 
----    with an account balance greater than the average account balance 
----    for all accounts of the same account type (checking or savings).
+-- -- 3. (18 points) List the accounts (by account number, in numeric order) 
+-- --    with an account balance greater than the average account balance 
+-- --    for all accounts of the same account type (checking or savings).
 SELECT A1.ACCNUMBER
 FROM ACCOUNT A1
 WHERE A1.ACCNUMBER >
@@ -45,13 +45,13 @@ WHERE A1.ACCNUMBER >
        WHERE A2.ACCTYPE=A1.ACCTYPE);
 
 
---- 4. (18 points) Display the transaction location and the average transaction 
----    amount for each transaction location where that average transaction amount
----    is less than the average transaction amount for all transactions.  Order the
----    results by average transaction amount, largest to smallest.
---- NOTE: for cleaner display, you can (but do not need to do so) CAST a real 
----    number to display with a limited number of decimal places; 
----    e.g. CAST (AVG(value) AS NUMBER(*,2)) 
+-- -- 4. (18 points) Display the transaction location and the average transaction 
+-- --    amount for each transaction location where that average transaction amount
+-- --    is less than the average transaction amount for all transactions.  Order the
+-- --    results by average transaction amount, largest to smallest.
+-- -- NOTE: for cleaner display, you can (but do not need to do so) CAST a real 
+-- --    number to display with a limited number of decimal places; 
+-- --    e.g. CAST (AVG(value) AS NUMBER(*,2)) 
 SELECT TRANSLOCATION,CAST (AVG(TRANSAMOUNT) AS NUMBER(*,2))
 FROM TRANSACTION
 GROUP BY TRANSLOCATION
@@ -62,10 +62,10 @@ ORDER BY AVG(TRANSAMOUNT) DESC;
 
 
 
---- 5. (16 points) Find each customer id, first name, and last name, plus the 
----    amount for the largest 'w' (withdrawal) transaction related to a 
----    savings account which that customer has opened.  Display the results 
----    in order by customer id.
+-- -- 5. (16 points) Find each customer id, first name, and last name, plus the 
+-- --    amount for the largest 'w' (withdrawal) transaction related to a 
+-- --    savings account which that customer has opened.  Display the results 
+-- --    in order by customer id.
 SELECT CUSTID,FNAME,LNAME,MAX(TRANSAMOUNT)
 FROM CUSTOMER C
 JOIN ACCOUNT A ON (A.CUSTOMER = C.CUSTID)
@@ -77,10 +77,10 @@ ORDER BY CUSTID;
 
 
 
---- 6. (17 points) List the customer id, first name and last name of any 
----    customers who have no active accounts.  This includes A) customers who 
----    have accounts, but they only have accounts that are closed and/or frozen, 
----    and also B) customers who do not have any accounts at all.
+-- -- 6. (17 points) List the customer id, first name and last name of any 
+-- --    customers who have no active accounts.  This includes A) customers who 
+-- --    have accounts, but they only have accounts that are closed and/or frozen, 
+-- --    and also B) customers who do not have any accounts at all.
 SELECT DISTINCT CUSTID,FNAME,LNAME
 FROM CUSTOMER C
 JOIN ACCOUNT A ON (A.CUSTOMER = C.CUSTID)
@@ -93,10 +93,10 @@ WHERE NOT EXISTS
       FROM ACCOUNT
       WHERE CUSTOMER = CUSTID);
 
---- 7. EXTRA CREDIT / ANSWER OPTIONAL (5 points): For each transaction location, 
----    display the number of accounts opened at that location, even if no 
----    accounts were opened at that location.
---- NOTE: at least one transaction location has no accounts opened at that location
+-- -- 7. EXTRA CREDIT / ANSWER OPTIONAL (5 points): For each transaction location, 
+-- --    display the number of accounts opened at that location, even if no 
+-- --    accounts were opened at that location.
+-- -- NOTE: at least one transaction location has no accounts opened at that location
 SELECT TRANSLOCATION, COUNT(DISTINCT A.ACCNUMBER)
 FROM TRANSACTION T
 JOIN ACCOUNT A ON (A.ACCNUMBER = T.ACCNUMBER)
