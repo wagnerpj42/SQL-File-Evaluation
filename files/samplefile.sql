0b9fe28e86004321b2d78e74cc5e2bd138de89be
Saksham <sthakur5@ncsu.edu>
2022-03-20 16:31:51 -0400

    Added tests for goThroughAllSubmissions function.

--- files/samplefile.sql
+++
@@ -1,9 +0,0 @@
--- -- Sample Assignment
--- -- Name: Sample Student
-
--- -- This is a placeholder file - please delete this file and replace with sample files or your own student submission files
-
--- -- 1. (100 points) List all columns for all customers
-
-SELECT *
-FROM Customer;
